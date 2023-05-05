031b93a3415efb4a21439e75b25dd9f96d2e5905
Mihail A <108764872+mihosal@users.noreply.github.com>
2023-05-05 11:22:15 +0300

    Add files via upload
    A small Improvement of the first edition of the program

---
+++ BMI_Calculator_V.1.1.py
@@ -0,0 +1,5 @@
+height = input("enter your height in m (use dot for decimal, for example 1.88): ")
+weight = input("enter your weight in kg: ")
+BMI = int(weight) / (float(height)**2)
+BMI_as_int = int(BMI)
+print("Your BMI is: ", BMI_as_int)
